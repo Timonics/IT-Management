76cb1e5a492b522d10c9ee886254a36392081416
Oderinde Michael <Olubiyioderinde7@gmail.com>
2025-09-17 12:11:37 +0100

    refactor: update layout classes in Header for improved responsiveness

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -14,8 +14,8 @@ const Header: React.FC = () => {
   //   console.log(currentPathname);
 
   return (
-    <div className="py-2 md:py-4 px-2 flex items-center justify-between">
-      <MenuSquare className="md:hidden" onClick={() => setIsOpen(true)} />
+    <div className="py-2 lg:py-4 px-2 flex items-center justify-between">
+      <MenuSquare className="lg:hidden md:size-10" onClick={() => setIsOpen(true)} />
       <h1 className="urban text-3xl md:text-5xl font-semibold">Dashboard</h1>
       <div className="flex items-center gap-4 mr-2">
         <div className="md:size-12 size-10 bg-black/5 rounded-full" />
